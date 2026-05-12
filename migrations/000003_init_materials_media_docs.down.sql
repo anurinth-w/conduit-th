2145db0ef09ed1a8a5b973048922eed727510a9d
anurinth-w <anurinth.w@gmail.com>
2026-05-13 00:13:14 +0700

    fix: remove coverprofile flag and fix migration down order

---
+++ migrations/000003_init_materials_media_docs.down.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS job_materials;
+DROP TABLE IF EXISTS documents;
+DROP TYPE  IF EXISTS document_status;
+DROP TYPE  IF EXISTS document_type;
+DROP TABLE IF EXISTS job_photos;
+DROP TYPE  IF EXISTS photo_stage;
+DROP TABLE IF EXISTS material_price_history;
+DROP TABLE IF EXISTS materials;
