eaa756c0c8e9f7d8f6e5b41e9b99362d0bc49cf4
anurinth-w <anurinth.w@gmail.com>
2026-05-13 00:16:49 +0700

    fix: use CASCADE in migration down to handle foreign key dependencies

--- migrations/000002_init_jobs.down.sql
+++ migrations/000002_init_jobs.down.sql
@@ -1,6 +1,6 @@
-DROP TABLE IF EXISTS job_assignments;
-DROP TABLE IF EXISTS job_code_sequences;
-DROP TABLE IF EXISTS jobs;
+DROP TABLE IF EXISTS job_assignments CASCADE;
+DROP TABLE IF EXISTS job_code_sequences CASCADE;
+DROP TABLE IF EXISTS jobs CASCADE;
 DROP TYPE  IF EXISTS assignment_status;
 DROP TYPE  IF EXISTS assignment_type;
 DROP TYPE  IF EXISTS job_status;
